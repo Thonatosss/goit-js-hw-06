63a8cd78217ee874eb9d1fbc42818899734f010d
Mykola Romashenko <sven4369@gmail.com>
2023-02-09 10:54:15 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,3 @@
-const refs = {
-  numberOfCatrgories: document.querySelectorAll("#categories .item"),
-};
-
-// console.log(`Number of categories: ${refs.numberOfCatrgories.length}`);
-
-// console.log(`Category: ${refs.parentList.childNodes[1].childNodes[1].textContent}`);
-// console.log(`Elements: ${refs.parentList.childNodes[1].childNodes[3].children.length}`);
-
-// console.log(`Category: ${refs.parentList.childNodes[3].childNodes[1].textContent}`);
-// console.log(`Elements: ${refs.parentList.childNodes[3].childNodes[3].children.length}`);
-
-// console.log(`Category: ${refs.parentList.childNodes[5].childNodes[1].textContent}`);
-// console.log(`Elements: ${refs.parentList.childNodes[5].childNodes[3].children.length}`);
 
 const categoriesList = document.querySelector("ul#categories");
 const categoriesItems = categoriesList.querySelectorAll("li.item");
